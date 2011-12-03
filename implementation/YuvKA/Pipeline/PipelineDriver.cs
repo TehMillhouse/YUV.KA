6d118e6e7fb69e2755178f529fb1aa62e32a04d9
Sebastian Ullrich <sebasti@nullrich.de>
2011-12-03 23:51:32 +0100

    Attempt at a PipelineDriver implementation
    "It compiles, therefore it works"

--- implementation/YuvKA/Pipeline/PipelineDriver.cs
+++ implementation/YuvKA/Pipeline/PipelineDriver.cs
@@ -10,15 +10,30 @@ namespace YuvKA.Pipeline
 {
 	public static class PipelineDriver
 	{
-		public static Task RenderFrame(PipelineGraph graph, IEnumerable<Node> startNodes, int frameIndex) 
+		public static Task RenderFrame(IEnumerable<Node> startNodes, int frameIndex) 
 		{
-			//ILookup<Node, Task> tasks = graph.Nodes.ToLookup(node => new Task(() => node.ProcessFrame(int frameIndex)));
-			throw new System.NotImplementedException();
+			var tasks = new Dictionary<Node, Task>();
+			IEnumerable<Task> startTasks = startNodes.Select(start => Visit(start, tasks, frameIndex));
+			return Task.Factory.ContinueWhenAll(startTasks.ToArray(), _ => { });
 		}
 
-		public static IEnumerable<Frame> RenderAllTheFrames(PipelineGraph graph, Node node, CancellationToken token)
+		private static Task Visit(Node node, Dictionary<Node, Task> tasks, int frameIndex)
 		{
-			throw new System.NotImplementedException();
+			Task result;
+			if (!tasks.TryGetValue(node, out result)) {
+				IEnumerable<Task> dependencies = node.Inputs.Select(i => Visit(i.Source.Node, tasks, frameIndex)); 
+				tasks.Add(node, result = Task.Factory.ContinueWhenAll(dependencies.ToArray(), _ => node.ProcessFrame(frameIndex)));
+			}
+			return result;
+		}
+
+		public static IEnumerable<Frame> RenderAllTheFrames(Node.Output output, int frameCount, CancellationToken token)
+		{
+			for (int i = 0; i < frameCount; i++) {
+				token.ThrowIfCancellationRequested();
+				RenderFrame(new[] { output.Node }, i).Wait(token);
+				yield return output.Buffer;
+			}
 		}
 	}
 }
