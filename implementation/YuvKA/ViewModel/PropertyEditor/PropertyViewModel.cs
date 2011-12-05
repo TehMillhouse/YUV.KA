b6322f896bba0f28c3982cc354ed44ad02c9f84d
Sebastian Ullrich <sebasti@nullrich.de>
2011-12-05 20:13:36 +0100

    Complete property editor view models

--- implementation/YuvKA/ViewModel/PropertyEditor/PropertyViewModel.cs
+++ implementation/YuvKA/ViewModel/PropertyEditor/PropertyViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel;
+using System.ComponentModel.Composition;
 
 namespace YuvKA.ViewModel.PropertyEditor
 {
@@ -10,5 +11,12 @@ namespace YuvKA.ViewModel.PropertyEditor
 	{
 		public object Source { get; private set; }
 		public PropertyDescriptor Property { get; private set; }
+		public object Value { get { throw new NotImplementedException(); } set { } }
+	}
+
+	[InheritedExport]
+	public abstract class PropertyViewModel<T> : PropertyViewModel
+	{
+		public new T Value { get { throw new NotImplementedException(); } set { } }
 	}
 }
---
+++ implementation/YuvKA/ViewModel/PropertyEditor/PropertyViewModels.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace YuvKA.ViewModel.PropertyEditor.Implementation
+{
+	public class BooleanPropertyViewModel : PropertyViewModel<bool> { }
+
+	public class PathPropertyViewModel : PropertyViewModel<Pipeline.FilePath>
+	{
+		public void OpenDialog()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class RgbPropertyViewModel : PropertyViewModel<VideoModel.Rgb>
+	{
+		public void OpenDialog()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class SizePropertyViewModel : PropertyViewModel<Pipeline.Size>
+	{
+		public int Width { get { throw new NotImplementedException(); } set { } }
+		public int Height { get { throw new NotImplementedException(); } set { } }
+	}
+
+	public abstract class NumericalPropertyViewModel<T> : PropertyViewModel<T>
+	{
+		public double? Minimum { get { throw new NotImplementedException(); } set { } }
+		public double? Maximum { get { throw new NotImplementedException(); } set { } }
+	}
+
+	public class IntPropertyViewModel : NumericalPropertyViewModel<int> { }
+
+	public class DoublePropertyViewModel : NumericalPropertyViewModel<double> { }
+
+	public class ObservableCollectionOfDoublePropertyViewModel : PropertyViewModel<ObservableCollection<double>> { }
+}
