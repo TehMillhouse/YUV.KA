12abd92d249d1c7970e558d1bb9c688c69a079b2
Robert Hangu <robert.hangu@gmail.com>
2011-12-05 14:08:24 +0100

    Add two nodes: - brightness, contrast, saturation - inverter

---
+++ implementation/YuvKA/Pipeline/Implementation/BrightnessContrastSaturationNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YuvKA.Pipeline.Implementation
+{
+    public class BrightnessContrastSaturationNode : YuvKA.Pipeline.Node
+    {
+        public int BrightnessLevel
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int ContrastLevel
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int SaturationLevel
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    
+        public override void ProcessFrame(int frameIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ implementation/YuvKA/Pipeline/Implementation/InverterNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YuvKA.Pipeline.Implementation
+{
+    public class InverterNode : YuvKA.Pipeline.Node
+    {
+        public override void ProcessFrame(int frameIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
