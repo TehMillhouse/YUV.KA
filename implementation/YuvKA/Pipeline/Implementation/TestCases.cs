ad6e0861f338fb4e43e6f803678c305be8aa06e8
Sebastian Ullrich <sebasti@nullrich.de>
2011-12-06 16:32:18 +0100

    Begin drafting global test cases

---
+++ implementation/YuvKA/Pipeline/Implementation/TestCases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YuvKA.ViewModel;
+
+namespace YuvKA.Pipeline.Implementation
+{
+    static class TestCases
+    {
+        static MainViewModel mainViewModel = new MainViewModel();
+
+        static public void T30()
+        {
+            mainViewModel.Save();
+            mainViewModel.Clear();
+            mainViewModel.Open();
+        }
+    }
+}
