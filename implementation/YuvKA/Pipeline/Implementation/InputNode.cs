35090ac2021053cf017dc1bbaeefdf1a5d90de47
Max Wagner <max.wagner01@gmail.com>
2011-12-08 23:51:07 +0100

    Work on entwurfsheft, provide template for expansion.

--- implementation/YuvKA/Pipeline/Implementation/InputNode.cs
+++ implementation/YuvKA/Pipeline/Implementation/InputNode.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 using YuvKA.VideoModel;
+using System.Runtime.Serialization;
 
 namespace YuvKA.Pipeline.Implementation
 {
+    [DataContract]
 	public abstract class InputNode : Node
 	{
+        [DataMember]
 		public Size Size { get; set; }
 
 		[Browsable(false)]
+        [DataMember]    
 		public virtual int FrameCount { get { return 1; } }
 	}
 }
