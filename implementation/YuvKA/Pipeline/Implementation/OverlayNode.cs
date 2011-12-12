df8cefb452a3f1cad5248baf98b985a51f1956fd
Michael Vollmer <michael.vollmer2@gmx.de>
2011-12-13 00:22:50 +0100

    Fix minor issues at outputnodes and add datacontract to OverlayNode

--- implementation/YuvKA/Pipeline/Implementation/OverlayNode.cs
+++ implementation/YuvKA/Pipeline/Implementation/OverlayNode.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using YuvKA.VideoModel;
 
 namespace YuvKA.Pipeline.Implementation
 {
+    [DataContract]
 	public class OverlayNode : OutputNode
 	{
+        [DataMember]
 		public IOverlayType Type { get; set; }
 
 		public override Frame[] ProcessFrame(Frame[] inputs, int frameIndex)
