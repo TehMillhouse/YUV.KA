c18e12afb11ef5c10bbf340fad855ad0cd10ac30
Sebastian Ullrich <sebasti@nullrich.de>
2011-12-27 20:20:15 +0100

    Add xUnit.net package & testing project

---
+++ implementation/YuvKA.Test/Pipeline/PipelineDriverTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+using YuvKA.Pipeline;
+
+namespace YuvKA.Test.Pipeline
+{
+	public class PipelineDriverTest
+	{
+		/// <summary>
+		/// Driver should not be implemented yet.
+		/// </summary>
+		[Fact]
+		public void NotImplemented()
+		{
+			Assert.Throws<NotImplementedException>(
+				() => new PipelineDriver().RenderTicks(null, 0, new CancellationToken())
+			);
+		}
+	}
+}
