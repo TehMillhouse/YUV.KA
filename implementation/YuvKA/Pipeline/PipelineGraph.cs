9f4c5aa7e4f1afe72f5e6c2259c115ed38fcfb08
Patrick Gemander <pgemander@web.de>
2011-12-29 16:49:15 +0100

    Implement DFS

--- implementation/YuvKA/Pipeline/PipelineGraph.cs
+++ implementation/YuvKA/Pipeline/PipelineGraph.cs
@@ -34,7 +34,44 @@ namespace YuvKA.Pipeline
 
 		public IEnumerable<Node> DepthFirstSearch(Node startNode)
 		{
-			throw new System.NotImplementedException();
+            LinkedList<Node> NodeList = new LinkedList<Node>();
+            HashSet<Node> Visited = new HashSet<Node>();
+
+            NodeList.AddLast(startNode);
+            Visited.Add(startNode);
+
+            foreach (Node.Input Input in startNode.Inputs)
+            {
+                Visit(Input.Source.Node, NodeList, Visited);
+            }
+
+            return NodeList;
 		}
+
+        private void Visit(Node node, LinkedList<Node> nodeList, HashSet<Node> visited) 
+        {
+
+            if (nodeList.Contains(node) && visited.Contains(node))
+            {
+				nodeList.AddLast(node);
+                return;
+            } 
+			else if (!nodeList.Contains(node)) 
+            {
+				nodeList.AddLast(node);
+				visited.Add(node);
+
+                foreach (Node.Input Input in node.Inputs) 
+                {
+                    Visit(Input.Source.Node, nodeList, visited);
+                }
+            }
+
+            visited.Remove(node);
+
+			return;
+        }
+
+
 	}
 }
