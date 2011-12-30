a0aaed33de23acb855184749e7e61a95bb274d8f
Patrick Gemander <pgemander@web.de>
2011-12-30 19:39:48 +0100

    Implement first version of PipelineGraph

--- implementation/YuvKA/Pipeline/PipelineGraph.cs
+++ implementation/YuvKA/Pipeline/PipelineGraph.cs
@@ -11,10 +11,11 @@ namespace YuvKA.Pipeline
 		{
 			get
 			{
-				throw new System.NotImplementedException();
+				return this.Nodes;
 			}
 			set
 			{
+				this.Nodes = value;
 			}
 		}
 
@@ -23,15 +24,46 @@ namespace YuvKA.Pipeline
 		{
 			get
 			{
-				throw new System.NotImplementedException();
+				int tickCount = 0;
+				foreach (Node node in Nodes) {
+					if (node is InputNode && ((InputNode)node).TickCount > tickCount) {
+						tickCount = ((InputNode)node).TickCount;
+					}
+				}
+				return tickCount;
 			}
 		}
 
+
+		/// <summary>
+		/// Returns true if the specified edge doesnt lead to a cycle. Else the method returns false.
+		/// </summary>
+		/// <param name="source"> The start of the new edge. </param>
+		/// <param name="sink"> The end of the new edge. </param>
+		/// <returns> True if the new edge has been added, else </returns>
 		public bool AddEdge(Node.Output source, Node.Input sink)
 		{
-			throw new System.NotImplementedException();
+			bool added = true;
+			if (source == null || sink == null) {
+				added = false;
+			}
+			else if (ContainsCycle(source.Node)) {
+				added = false;
+			}
+			else {
+				sink.Source = source;
+			}
+
+			return added;
 		}
 
+		/// <summary>
+		/// Returns a list which contains those Nodes who are necessary to let the specified Node process.
+		/// If the Graph contains a cycle, the returned list will contain one of the cycle-Nodes twice.
+		/// Otherwise every Node in the list is unique.
+		/// </summary>
+		/// <param name="startNode"> The specified Node. </param>
+		/// <returns> Returns the list of necessary Nodes.</returns>
 		public IEnumerable<Node> DepthFirstSearch(Node startNode)
 		{
 			LinkedList<Node> nodeList = new LinkedList<Node>();
@@ -47,26 +79,46 @@ namespace YuvKA.Pipeline
 			return nodeList;
 		}
 
+		/**
+		 * Recursive part of the depth first search.
+		 */
 		private void Visit(Node node, LinkedList<Node> nodeList, HashSet<Node> visited)
 		{
 			if (nodeList.Contains(node) && visited.Contains(node)) {
 				nodeList.AddLast(node);
+				//Cancel further search here, since a cycle has been found
 				return;
 			}
-			else {
-				if (!nodeList.Contains(node)) {
-					nodeList.AddLast(node);
-					visited.Add(node);
+			else if (!nodeList.Contains(node)) {
+				nodeList.AddLast(node);
+				visited.Add(node);
 
-					foreach (Node.Input input in node.Inputs) {
-						Visit(input.Source.Node, nodeList, visited);
-					}
+				foreach (Node.Input input in node.Inputs) {
+					Visit(input.Source.Node, nodeList, visited);
 				}
 			}
 
 			visited.Remove(node);
 
 			return;
+		}
+
+		//Returns true, if a cycle that includes the specified Node exists.
+		private bool ContainsCycle(Node startNode)
+		{
+			IEnumerator<Node> enumerator = DepthFirstSearch(startNode).GetEnumerator();
+			bool containsCycle = false;
+
+			while (enumerator.MoveNext()) {
+				IEnumerator<Node> comparator = enumerator;
+				while (comparator.MoveNext()) {
+					if (enumerator == comparator) {
+						containsCycle = true;
+					}
+				}
+			}
+
+			return containsCycle;
 		}
 	}
 }
