6cee0911fc06a03ebbbc37c8dbf7d8f4db6c528f
Max Wagner <max.wagner01@gmail.com>
2011-12-31 14:35:58 +0100

    [videomodel:test] Add resource/output dirs to code, .gitignore
    Also remove obsolete test

--- implementation/YuvKA.Test/Pipeline/YuvEncoderTest.cs
+++ implementation/YuvKA.Test/Pipeline/YuvEncoderTest.cs
@@ -12,32 +12,15 @@ namespace YuvKA.Test.Pipeline
 	public class YuvEncoderTest
 	{
 		/// <summary>
-		/// This just tests the Yuv2Rgb conversion for a given file.
-		/// It converts the first frame of a yuv video and saves it as an image
+		/// Tests the Video object's ability to load videos from yuv files, convert them, and provide correct frame objects
 		/// </summary>
-		[Fact]
-		public void TestYuvConversion()
-		{
-			int width = 352;
-			int height = 240; // be sure to adjust this when running the test with your yuv file, lest there be colorful gibberish
-			string loadFile = ""; // Insert path to your yuv
-			string saveFile = ""; // Insert path to where you want the converted version saved
-			FileStream fs = new FileStream(loadFile, FileMode.Open);
-			byte[] data = new byte[fs.Length];
-			fs.Read(data, 0, (int)fs.Length);
-			fs.Close();
-			Frame frame = YuvEncoder.Yuv2Rgb(data, width, height);
-			Bitmap bmp = Frame2Bitmap(frame);
-			bmp.Save(saveFile);
-		}
-
 		[Fact]
 		public void TestVideoFunctionality()
 		{
 			int width = 352;
 			int height = 240;
-			string fileName = "...\\americanFootball_352x240_125.yuv"; // be sure to adjust this beforehand
-			string saveName = ""; // warning, depending on the file, this produces a lot of images
+			string fileName = "..\\..\\..\\..\\resources\\americanFootball_352x240_125.yuv"; // be sure to adjust this beforehand
+			string saveName = "..\\..\\..\\..\\output\\"; // warning, depending on the file, this produces a lot of images
 			// leave the file extension away
 			YuvEncoder.Video video = new YuvEncoder.Video(fileName, null, new VideoModel.Size(width, height));
 			// Don't expect this to work if the filename doesn't happen to be formatted
@@ -45,13 +28,17 @@ namespace YuvKA.Test.Pipeline
 			Assert.Equal(video.FrameCount, int.Parse(fileName.Substring(fileName.Length - 7, 3)));
 
 			Bitmap bmp;
-			for (int i = 0; i < video.FrameCount; i++) {
+			// I test the first and last three frames
+			for (int i = 0; i < 3; i++) {
+				bmp = Frame2Bitmap(video[i]);
+				bmp.Save(saveName + i.ToString() + ".png");
+			}
+			for (int i = video.FrameCount - 4; i < video.FrameCount; i++) {
 				bmp = Frame2Bitmap(video[i]);
 				bmp.Save(saveName + i.ToString() + ".png");
 			}
 		}
 
-		[Fact(Skip = "This is just a helper method for testing")]
 		private static Bitmap Frame2Bitmap(Frame frame)
 		{
 			Bitmap bmp = new Bitmap(frame.Size.Width, frame.Size.Height);
