a813202e904aef42171ff18bb36e6151d0cc301b
Michael Vollmer <michael.vollmer2@gmx.de>
2012-01-02 22:41:47 +0100

    [manipnodes] Improve Gaussian Blur Improve speed due to separability Fix the Brightnessloss by calculating in floats instead of bytes

--- implementation/YuvKA.Implementation/Pipeline/BlurNode.cs
+++ implementation/YuvKA.Implementation/Pipeline/BlurNode.cs
@@ -64,28 +64,47 @@ namespace YuvKA.Pipeline.Implementation
 
 		private Frame GaussianBlur(Frame input, int tick)
 		{
+			/* Since this Array is sort of ugly now: workspace[BlurringDimension, x-coord, y-coord, colorchannel] */
+			float[, , , ] workspace = new float[2, input.Size.Width, input.Size.Height, 3];
+			/* Blur horinzontal dimension */
+			for (int x = 0; x < input.Size.Width; x++) {
+				for (int y = 0; y < input.Size.Height; y++) {
+					workspace[0, x, y, 0] = workspace[0, x, y, 1] = workspace[0, x, y, 2] = 0F;
+					for (int z = x - (3 * Radius); z <= x + (3 * Radius); z++) {
+						float factor = G(z - x);
+						Rgb imagePixel = GetCappedPixels(z, y, input);
+						workspace[0, x, y, 0] += factor * imagePixel.R;
+						workspace[0, x, y, 1] += factor * imagePixel.G;
+						workspace[0, x, y, 2] += factor * imagePixel.B;
+					}
+				}
+			}
+			/* Blur vertical dimension */
+			for (int x = 0; x < input.Size.Width; x++) {
+				for (int y = 0; y < input.Size.Height; y++) {
+					workspace[1, x, y, 0] = workspace[1, x, y, 1] = workspace[1, x, y, 2] = 0F;
+					for (int z = y - (3 * Radius); z <= y + (3 * Radius); z++) {
+						float factor = G(y - z);
+						int cappedY = Math.Min(input.Size.Height - 1, Math.Max(0, z));
+						workspace[1, x, y, 0] += factor * workspace[0, x, cappedY, 0];
+						workspace[1, x, y, 1] += factor * workspace[0, x, cappedY, 1];
+						workspace[1, x, y, 2] += factor * workspace[0, x, cappedY, 2];
+					}
+				}
+			}
+			/* Convert floatarray to frame */
 			Frame result = new Frame(input.Size);
 			for (int x = 0; x < input.Size.Width; x++) {
 				for (int y = 0; y < input.Size.Height; y++) {
-					result[x, y] = new Rgb(0, 0, 0);
-					for (int xi = x - (3 * Radius); xi <= x + (3 * Radius); xi++) {
-						for (int yi = y - (3 * Radius); yi <= y + (3 * Radius); yi++) {
-							double curG = G(xi - x, yi - y);
-							Rgb imagePixel = GetCappedPixels(xi, yi, input);
-							int newR = (int)(result[x, y].R + curG * imagePixel.R);
-							int newG = (int)(result[x, y].G + curG * imagePixel.G);
-							int newB = (int)(result[x, y].B + curG * imagePixel.B);
-							result[x, y] = new Rgb((byte)newR, (byte)newG, (byte)newB);
-						}
-					}
+					result[x, y] = new Rgb((byte)workspace[1, x, y, 0], (byte)workspace[1, x, y, 1], (byte)workspace[1, x, y, 2]);
 				}
 			}
 			return result;
 		}
 
-		private double G(int x, int y)
+		private float G(int x)
 		{
-			return (1 / (2 * Math.PI * Radius * Radius)) * Math.Pow(Math.E, -1 * (((double) x * x + y * y) / (2 * Radius * Radius)));
+			return (float)((1 / Math.Sqrt(2 * Math.PI * Radius * Radius)) * Math.Pow(Math.E, -1 * (((double)x * x) / (2 * Radius * Radius))));
 		}
 
 		private Rgb GetCappedPixels(int x, int y, Frame frame)
