ece6d65a04440c1bba9c9515b4f355e5b6dce366
Sebastian Ullrich <sebasti@nullrich.de>
2012-01-03 16:24:14 +0100

    [PipelineState] Implement properties so they can be serialized
    Warning, some of these setters should probably not be empty.

--- implementation/YuvKA/Pipeline/PipelineState.cs
+++ implementation/YuvKA/Pipeline/PipelineState.cs
@@ -9,43 +9,16 @@ namespace YuvKA.Pipeline
 	public class PipelineState
 	{
 		[DataMember]
-		public int CurrentTick
-		{
-			get
-			{
-				throw new System.NotImplementedException();
-			}
-			set
-			{
-			}
-		}
+		public int CurrentTick { get; set; }
 
 		/// <summary>
 		/// Replay speed in frames per second
 		/// </summary>
 		[DataMember]
-		public int Speed
-		{
-			get
-			{
-				throw new System.NotImplementedException();
-			}
-			set
-			{
-			}
-		}
+		public int Speed { get; set; }
 
 		[DataMember]
-		public PipelineGraph Graph
-		{
-			get
-			{
-				throw new System.NotImplementedException();
-			}
-			set
-			{
-			}
-		}
+		public PipelineGraph Graph { get; private set; }
 
 		[Import(typeof(IEventAggregator))]
 		public IEventAggregator Events { get; set; }
