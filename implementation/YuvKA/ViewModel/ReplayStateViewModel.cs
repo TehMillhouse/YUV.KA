f0a6d3f80758844433026cdeffd2ef282da50766
Sebastian Ullrich <sebasti@nullrich.de>
2012-01-04 01:51:35 +0100

    [ReplayStateViewModel] Implement

--- implementation/YuvKA/ViewModel/ReplayStateViewModel.cs
+++ implementation/YuvKA/ViewModel/ReplayStateViewModel.cs
@@ -1,34 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using YuvKA.Pipeline;
+﻿using System.Linq;
 
 namespace YuvKA.ViewModel
 {
 	public class ReplayStateViewModel
 	{
-		public bool IsPlaying { get { throw new NotImplementedException(); } }
-		public MainViewModel Parent { get { throw new NotImplementedException(); } }
-	
+		public ReplayStateViewModel(MainViewModel parent)
+		{
+			Parent = parent;
+		}
+
+		public bool IsPlaying { get; private set; }
+		public MainViewModel Parent { get; private set; }
+
 		public void Slower()
 		{
-			throw new System.NotImplementedException();
+			if (Parent.Model.Speed > 1)
+				Parent.Model.Speed /= 2;
 		}
 
 		public void PlayPause()
 		{
-			Parent.Model.Start(Parent.OpenWindows.Select(w => w.NodeModel));
+			if (Parent.Model.CurrentTick == Parent.Model.Graph.TickCount - 1)
+				Stop();
+
+			if (IsPlaying)
+				Parent.Model.Start(Parent.OpenWindows.Select(w => w.NodeModel));
+			else
+				Parent.Model.Stop();
+			IsPlaying = !IsPlaying;
 		}
 
 		public void Stop()
 		{
-			throw new System.NotImplementedException();
+			Parent.Model.CurrentTick = 0;
+			IsPlaying = false;
 		}
 
 		public void Faster()
 		{
-			throw new System.NotImplementedException();
+			Parent.Model.Speed *= 2;
 		}
 	}
 }
