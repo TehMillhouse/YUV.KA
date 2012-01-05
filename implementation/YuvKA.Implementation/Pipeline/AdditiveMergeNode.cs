2e5238c4a8a180f95bf9c34568e60e2380e00861
Michael Vollmer <michael.vollmer2@gmx.de>
2012-01-05 15:20:29 +0100

    [manipnodes] Implement AdditiveMergeNode

--- implementation/YuvKA.Implementation/Pipeline/AdditiveMergeNode.cs
+++ implementation/YuvKA.Implementation/Pipeline/AdditiveMergeNode.cs
@@ -14,7 +14,29 @@ namespace YuvKA.Pipeline.Implementation
 
 		public override Frame[] Process(Frame[] inputs, int tick)
 		{
-			throw new NotImplementedException();
+			int maxX = 0;
+			int maxY = 0;
+			for (int i = 0; i < inputs.Length; i++) {
+				maxX = Math.Max(maxX, inputs[i].Size.Width);
+				maxY = Math.Max(maxY, inputs[i].Size.Height);
+			}
+			Frame[] output = { new Frame(new Size(maxX, maxY)) };
+			for (int i = 0; i < inputs.Length; i++) {
+				for (int x = 0; x < maxX; x++) {
+					for (int y = 0; y < maxY; y++) {
+						byte newR = (byte)Math.Min(255, output[0][x, y].R + GetPixel(x, y, inputs[i]).R);
+						byte newG = (byte)Math.Min(255, output[0][x, y].G + GetPixel(x, y, inputs[i]).G);
+						byte newB = (byte)Math.Min(255, output[0][x, y].B + GetPixel(x, y, inputs[i]).B);
+						output[0][x, y] = new Rgb(newR, newG, newB);
+					}
+				}
+			}
+			return output;
+		}
+
+		private Rgb GetPixel(int x, int y, Frame input)
+		{
+			return (x > input.Size.Width || y > input.Size.Height || x < 0 || y < 0) ? new Rgb(0, 0, 0) : input[x, y];
 		}
 	}
 }
