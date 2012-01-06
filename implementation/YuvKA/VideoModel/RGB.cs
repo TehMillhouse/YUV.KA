f0df982009bfb65dd6b19dcc15134e9048dc4359
Michael Vollmer <michael.vollmer2@gmx.de>
2012-01-06 14:24:49 +0100

    [videomodel] Override Rgb.Equals and ==

--- implementation/YuvKA/VideoModel/RGB.cs
+++ implementation/YuvKA/VideoModel/RGB.cs
@@ -12,5 +12,39 @@
 		public byte R { get; private set; }
 		public byte G { get; private set; }
 		public byte B { get; private set; }
+
+		public static bool operator ==(Rgb a, Rgb b)
+		{
+			// If both are null, or both are same instance, return true.
+			if (object.ReferenceEquals(a, b)) {
+				return true;
+			}
+
+			// If one is null, but not both, return false.
+			if (((object)a == null) || ((object)b == null)) {
+				return false;
+			}
+
+			// Return true if the fields match:
+			return a.R == b.R && a.G == b.G && a.B == b.B;
+		}
+
+		public static bool operator !=(Rgb a, Rgb b)
+		{
+			return !(a == b);
+		}
+
+		public override int GetHashCode()
+		{
+			return R ^ G ^ B;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Rgb))
+				return false;
+			Rgb pixel = (Rgb)obj;
+			return (R == pixel.R && G == pixel.G && B == pixel.B);
+		}
 	}
 }
