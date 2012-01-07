2c89b22cbcb10545a66a3b042da25265ec205001
Robert Hangu <robert.hangu@gmail.com>
2012-01-07 23:55:41 +0100

    [inputnodes] Implement ImageInputNode and add a test for it

--- implementation/YuvKA.Implementation/Pipeline/ImageInputNode.cs
+++ implementation/YuvKA.Implementation/Pipeline/ImageInputNode.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Drawing;
 using System.Runtime.Serialization;
 using YuvKA.VideoModel;
 
@@ -8,17 +8,62 @@ namespace YuvKA.Pipeline.Implementation
 	[DataContract]
 	public class ImageInputNode : InputNode
 	{
-		public ImageInputNode() : base(outputCount: 1)
+		Bitmap inputImage;
+		FilePath fileName;
+
+		public ImageInputNode()
+			: base(outputCount: 1)
 		{
 		}
 
 		[DisplayName("File Name")]
 		[DataMember]
-		public FilePath FileName { get; set; }
+		public FilePath FileName
+		{
+			get { return fileName; }
+			set
+			{
+				fileName = value;
+				inputImage = null;
+			}
+		}
 
+		/// <summary>
+		/// Returns a new Frame with the RGB data of the selected PNG image
+		/// </summary>
+		/// <param name="tick">Parameter ignored.</param>
+		/// <returns>A Frame with the RGB information of the selected image.</returns>
 		public override Frame OutputFrame(int tick)
 		{
-			throw new NotImplementedException();
+			EnsureInputLoaded();
+			Frame outputFrame = new Frame(new YuvKA.VideoModel.Size(inputImage.Width, inputImage.Height));
+
+			for (int y = 0; y < outputFrame.Size.Height; ++y) {
+				for (int x = 0; x < outputFrame.Size.Width; ++x) {
+					outputFrame[x, y] = new Rgb(inputImage.GetPixel(x, y).R, inputImage.GetPixel(x, y).G, inputImage.GetPixel(x, y).B);
+				}
+			}
+
+			return outputFrame;
+		}
+
+		protected override void OnSizeChanged()
+		{
+			base.OnSizeChanged();
+			if (inputImage != null) {
+				inputImage = new Bitmap(inputImage, new System.Drawing.Size(Size.Width, Size.Height));
+			}
+		}
+
+		private void EnsureInputLoaded()
+		{
+			if (inputImage == null) {
+				inputImage = new Bitmap(FileName.Path);
+
+				if (inputImage.Height != Size.Height || inputImage.Width != Size.Width) {
+					OnSizeChanged();
+				}
+			}
 		}
 	}
 }
---
+++ implementation/YuvKA.Test/Pipeline/InputNodesTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using Xunit;
+using YuvKA.Pipeline.Implementation;
+using YuvKA.VideoModel;
+
+namespace YuvKA.Test.Pipeline
+{
+	public class InputNodesTests
+	{
+		// Read a PNG file and resize it using the methods in ImageInputNode.
+		// This test does no verification. It's intended to generate a result to be visualized.
+		[Fact]
+		public void ImageInputTest()
+		{
+			ImageInputNode inputNode = new ImageInputNode();
+			inputNode.Size = new YuvKA.VideoModel.Size(100, 80);
+			inputNode.FileName = new YuvKA.Pipeline.FilePath("..\\..\\..\\..\\resources\\bmp.png");
+			Frame outputFrame = inputNode.OutputFrame(0);
+
+			Bitmap outputImage = new Bitmap(inputNode.Size.Width, inputNode.Size.Height);
+			for (int y = 0; y < outputFrame.Size.Height; ++y) {
+				for (int x = 0; x < outputFrame.Size.Width; ++x) {
+					outputImage.SetPixel(x, y, Color.FromArgb(outputFrame[x, y].R, outputFrame[x, y].G, outputFrame[x, y].B));
+				}
+			}
+			outputImage.Save("..\\..\\..\\..\\output\\bmp-resized-" +
+							inputNode.Size.Width + "-" + inputNode.Size.Height + ".png");
+
+			// Enlarge the image
+			inputNode.Size = new YuvKA.VideoModel.Size(200, 400);
+			outputFrame = inputNode.OutputFrame(0);
+			outputImage = new Bitmap(inputNode.Size.Width, inputNode.Size.Height);
+			for (int y = 0; y < outputFrame.Size.Height; ++y) {
+				for (int x = 0; x < outputFrame.Size.Width; ++x) {
+					outputImage.SetPixel(x, y, Color.FromArgb(outputFrame[x, y].R, outputFrame[x, y].G, outputFrame[x, y].B));
+				}
+			}
+			outputImage.Save("..\\..\\..\\..\\output\\bmp-resized-" +
+							inputNode.Size.Width + "-" + inputNode.Size.Height + ".png");
+
+			Assert.True(true);
+		}
+	}
+}
