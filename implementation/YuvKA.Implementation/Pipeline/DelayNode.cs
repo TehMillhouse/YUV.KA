8e693560fc81c221bb59dd6d4f617fd78dcedd6b
Michael Vollmer <michael.vollmer2@gmx.de>
2012-01-09 02:57:21 +0100

    [manipnodes] Implement DelayNode

--- implementation/YuvKA.Implementation/Pipeline/DelayNode.cs
+++ implementation/YuvKA.Implementation/Pipeline/DelayNode.cs
@@ -21,7 +21,21 @@ namespace YuvKA.Pipeline.Implementation
 
 		public override Frame[] Process(Frame[] inputs, int tick)
 		{
-			throw new NotImplementedException();
+			if (queue.Count != Delay) {
+				Frame blackFrame = new Frame(inputs[0].Size);
+				for (int x = 0; x < inputs[0].Size.Width; x++) {
+					for (int y = 0; y < inputs[0].Size.Height; y++) {
+						blackFrame[x, y] = new Rgb(0, 0, 0);
+					}
+				}
+				queue = new Queue<Frame>();
+				for (int i = 0; i < Delay; i++) {
+					queue.Enqueue(blackFrame);
+				}
+			}
+			queue.Enqueue(inputs[0]);
+			Frame[] returnFrame = { queue.Dequeue() };
+			return returnFrame;
 		}
 	}
 }
