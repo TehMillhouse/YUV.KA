b7a8e055e09d3a18225a193ac6969e507caa4fbc
Sebastian Ullrich <sebasti@nullrich.de>
2012-01-10 18:51:55 +0100

    [PipelineState] Implement

--- implementation/YuvKA/Pipeline/FrameRenderedMessage.cs
+++ implementation/YuvKA/Pipeline/FrameRenderedMessage.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using YuvKA.VideoModel;
 
 namespace YuvKA.Pipeline
 {
-    public class TickRenderedMessage
-    {
-        public Frame this[Node.Output output]
-        {
-            get { throw new NotImplementedException(); }
-        }
-    }
+	public class TickRenderedMessage
+	{
+		IDictionary<Node.Output, Frame> dic;
+
+		public TickRenderedMessage(IDictionary<Node.Output, Frame> dic)
+		{
+			this.dic = dic;
+		}
+
+		public Frame this[Node.Output output] { get { return dic[output]; } }
+	}
 }
--- implementation/YuvKA/Pipeline/PipelineState.cs
+++ implementation/YuvKA/Pipeline/PipelineState.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Reactive.Linq;
 using System.Runtime.Serialization;
+using System.Threading;
 using Caliburn.Micro;
 
 namespace YuvKA.Pipeline
 {
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable", Justification = "Long-living, rarely instantiated class")]
 	[DataContract]
 	public class PipelineState
 	{
+		PipelineDriver driver = new PipelineDriver();
+		CancellationTokenSource cts;
+		DateTimeOffset? lastTick;
+
 		public PipelineState()
 		{
 			Graph = new PipelineGraph();
@@ -26,21 +34,37 @@ namespace YuvKA.Pipeline
 		public PipelineGraph Graph { get; private set; }
 
 		[Import]
-		public IEventAggregator Events { get; private set; }
+		IEventAggregator Events { get; set; }
 
 		public void Start(IEnumerable<Node> outputNodes)
 		{
-			//PipelineDriver.RenderTicks(outputNodes, CurrentTick, null);
+			RenderTicks(outputNodes, Graph.TickCount);
 		}
 
 		public void Stop()
 		{
-			throw new System.NotImplementedException();
+			if (cts != null)
+				cts.Cancel();
 		}
 
 		public void RenderTick(IEnumerable<Node> outputNodes)
 		{
-			//PipelineDriver.RenderTicks(outputNodes, CurrentTick, null);
+			RenderTicks(outputNodes, tickCount: 1);
+		}
+
+		private void RenderTicks(IEnumerable<Node> outputNodes, int tickCount)
+		{
+			cts = new CancellationTokenSource();
+			driver.RenderTicks(outputNodes, CurrentTick, tickCount, cts).ForEach(dic => {
+				if (lastTick.HasValue) {
+					DateTimeOffset nextTick = lastTick.Value + TimeSpan.FromSeconds(1.0 / Speed);
+					if (DateTimeOffset.Now < nextTick)
+						Thread.Sleep(nextTick - lastTick.Value);
+					lastTick = nextTick;
+				}
+				Events.Publish(new TickRenderedMessage(dic));
+				CurrentTick++;
+			});
 		}
 	}
 }
