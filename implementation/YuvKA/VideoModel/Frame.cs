416a9545a9d342e8eba5110b5be0395767c6d5cf
PGemander <pgemander@web.de>
2012-01-11 19:03:53 +0100

    [Frame] Add MaxBoundaries method

--- implementation/YuvKA/VideoModel/Frame.cs
+++ implementation/YuvKA/VideoModel/Frame.cs
@@ -1,4 +1,6 @@
-﻿namespace YuvKA.VideoModel
+﻿using System;
+
+namespace YuvKA.VideoModel
 {
 	public class Frame
 	{
@@ -13,8 +15,7 @@
 		// Alternate constructor for saving the trouble of using the indexer for everything
 		public Frame(Size size, Rgb[] data)
 		{
-			if (data.Length != size.Height * size.Width)
-			{
+			if (data.Length != size.Height * size.Width) {
 				throw new System.ArgumentException();
 			}
 			this.data = data;
@@ -32,6 +33,22 @@
 		public Rgb GetPixelOrBlack(int x, int y)
 		{
 			return (x < Size.Width && y < Size.Height) ? this[x, y] : new Rgb(0, 0, 0);
+		}
+
+		/// <summary>
+		/// Returns the largest boundaries found in the specified frame array, so all frame sizes are smaller than the returned one.
+		/// </summary>
+		/// <param name="frames">The specified frame array.</param>
+		/// <returns>The size object containing the larges boundaries.</returns>
+		public static Size MaxBoundaries(Frame[] frames)
+		{
+			int maxX = 0;
+			int maxY = 0;
+			foreach (Frame frame in frames) {
+				maxX = Math.Max(maxX, frame.Size.Width);
+				maxY = Math.Max(maxY, frame.Size.Height);
+			}
+			return new Size(maxX, maxY);
 		}
 	}
 }
