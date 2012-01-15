a2bd26d9ec44cf0c7d7e4545140eabdca5d9dfe1
Michael Vollmer <Michael.Vollmer2@gmx.de>
2012-01-15 18:59:45 +0100

    [OutputNodes] Change Gridcolor at Blockoverlay to black

--- implementation/YuvKA.Implementation/Pipeline/OverlayTypes.cs
+++ implementation/YuvKA.Implementation/Pipeline/OverlayTypes.cs
@@ -94,63 +94,63 @@ namespace YuvKA.Pipeline.Implementation
 			switch (decision) {
 				case MacroblockPartitioning.Inter16x16:
 					if ((x % 16 == 0) || (y % 16 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return pixel;
 				case MacroblockPartitioning.Inter16x8:
 					if ((x % 16 == 0) || (y % 8 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return pixel;
 				case MacroblockPartitioning.Inter4x4:
 					if ((x % 4 == 0) || (y % 4 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return pixel;
 				case MacroblockPartitioning.Inter4x8:
 					if ((x % 4 == 0) || (y % 8 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return pixel;
 				case MacroblockPartitioning.Inter8x16:
 					if ((x % 8 == 0) || (y % 16 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return pixel;
 				case MacroblockPartitioning.Inter8x4:
 					if ((x % 8 == 0) || (y % 4 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return pixel;
 				case MacroblockPartitioning.Inter8x8:
 					if ((x % 8 == 0) || (y % 8 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return pixel;
 				/* TODO Ask Kobbe what this is */
 				case MacroblockPartitioning.Inter8x8OrBelow:
 					if ((x % 8 == 0) || (y % 8 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return pixel;
 				case MacroblockPartitioning.Intra16x16:
 					if ((x % 16 == 0) || (y % 16 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return new Rgb((byte)Math.Max(255, pixel.R + 40), pixel.G, pixel.B);
 				case MacroblockPartitioning.Intra4x4:
 					if ((x % 4 == 0) || (y % 4 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return new Rgb((byte)Math.Max(255, pixel.R + 40), pixel.G, pixel.B);
 				case MacroblockPartitioning.Intra8x8:
 					if ((x % 8 == 0) || (y % 8 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return new Rgb((byte)Math.Max(255, pixel.R + 40), pixel.G, pixel.B);
 				default:
 					if ((x % 16 == 0) || (y % 16 == 0))
-						return new Rgb(255, 255, 255);
+						return new Rgb(0, 0, 0);
 					else
 						return new Rgb(pixel.R, pixel.G, (byte)Math.Max(255, pixel.B + 40));
 			}
