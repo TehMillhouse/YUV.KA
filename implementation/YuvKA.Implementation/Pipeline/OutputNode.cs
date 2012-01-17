0c175f762d806395c57ed31c1ac84bfa7a8ed62c
Max Wagner <max.wagner01@gmail.com>
2012-01-17 10:39:36 +0100

    [outputnodes] Make abstract OutputNode class serializable

--- implementation/YuvKA.Implementation/Pipeline/OutputNode.cs
+++ implementation/YuvKA.Implementation/Pipeline/OutputNode.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using YuvKA.VideoModel;
 
 namespace YuvKA.Pipeline.Implementation
 {
+	[DataContract]
 	public abstract class OutputNode : Node
 	{
 		public OutputNode(int? inputCount) : base(inputCount: inputCount, outputCount: 0)
