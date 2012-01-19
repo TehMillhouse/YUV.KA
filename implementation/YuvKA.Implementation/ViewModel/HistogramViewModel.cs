db62aabaa222a15cd6c0effb849bcd78d7fbd2e0
Sebastian Ullrich <sebasti@nullrich.de>
2012-01-19 20:33:12 +0100

    [HistogramView] Fix & simplify
    Still doesn't work because of some exception inside of WPF Toolkit

--- implementation/YuvKA.Implementation/ViewModel/HistogramViewModel.cs
+++ implementation/YuvKA.Implementation/ViewModel/HistogramViewModel.cs
@@ -4,34 +4,22 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using YuvKA.Pipeline.Implementation;
 using YuvKA.Pipeline;
+using System.Linq;
 
 namespace YuvKA.ViewModel.Implementation
 {
 	public class HistogramViewModel : OutputWindowViewModel
 	{
-		private HistogramNodeData model = new HistogramNodeData();
+		public HistogramViewModel(HistogramNode nodeModel)
+			: base(nodeModel) { }
 
-		public HistogramViewModel(Node nodeModel) : base(nodeModel)
-		{
-			data = model;
-		}
-
-		public HistogramNode Node { get; set; }
-
-		private ObservableCollection<KeyValuePair<int, double>> data;
-
-		public ObservableCollection<KeyValuePair<int, double>> Data
-		{
-			get { return data; }
-		}
-
-		public new HistogramNode NodeModel { get; set; }
+		public new HistogramNode NodeModel { get { return (HistogramNode)base.NodeModel; } }
+		public IEnumerable<KeyValuePair<int, double>> Data { get; private set; }
 
 		public override void Handle(Pipeline.TickRenderedMessage message)
 		{
-			model.Clear();
-			model.setData(Node.Data);
-			data = model;
+			Data = NodeModel.Data.Select((datum, idx) => new KeyValuePair<int, double>(idx, datum));
+			NotifyOfPropertyChange(() => Data);
 		}
 	}
 }
