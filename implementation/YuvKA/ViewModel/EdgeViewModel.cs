a9c37de72740f0b652cce87ac4e5a1af911bab78
Sebastian Ullrich <sebasti@nullrich.de>
2012-01-20 16:46:07 +0100

    [EdgeVM] Improve (maybe) edge bending strategy

--- implementation/YuvKA/ViewModel/EdgeViewModel.cs
+++ implementation/YuvKA/ViewModel/EdgeViewModel.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Windows.Media;
 using Caliburn.Micro;
+using YuvKA.Pipeline;
 
 namespace YuvKA.ViewModel
 {
@@ -62,13 +63,16 @@ namespace YuvKA.ViewModel
 		{
 			get
 			{
-				Vector delta = EndPoint - StartPoint;
-				Vector horizDelta = new Vector(delta.X, 0);
-
 				var geo = new StreamGeometry();
+				var delta = StartPoint - EndPoint;
+				var angle = Math.Atan2(delta.Y, delta.X);
+				var controlOffset = new Vector(Math.Abs(angle) / Math.PI * 200, 0);
+				if (StartViewModel.Model is Node.Input)
+					controlOffset *= -1;
 				using (var ctx = geo.Open()) {
 					ctx.BeginFigure(StartPoint, isFilled: false, isClosed: false);
-					ctx.BezierTo(StartPoint + horizDelta / 2, EndPoint - horizDelta / 2, EndPoint, isStroked: true, isSmoothJoin: false);
+					ctx.BezierTo(StartPoint + controlOffset, EndPoint - controlOffset, EndPoint,
+						isStroked: true, isSmoothJoin: false);
 				}
 				return geo;
 			}
