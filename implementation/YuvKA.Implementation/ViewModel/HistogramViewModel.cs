e28b3f3d2804330d83c6d97782a7d4278f88d26e
Sebastian Ullrich <sebasti@nullrich.de>
2012-01-20 15:40:13 +0100

    [HistogramVM] Fix data never being set

--- implementation/YuvKA.Implementation/ViewModel/HistogramViewModel.cs
+++ implementation/YuvKA.Implementation/ViewModel/HistogramViewModel.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Microsoft.Research.DynamicDataDisplay.DataSources;
 using YuvKA.Pipeline.Implementation;
-using YuvKA.Pipeline;
-using System.Linq;
 
 namespace YuvKA.ViewModel.Implementation
 {
@@ -17,40 +14,15 @@ namespace YuvKA.ViewModel.Implementation
 		}
 
 		public new HistogramNode NodeModel { get { return (HistogramNode)base.NodeModel; } }
-		private EnumerableDataSource<KeyValuePair<int, double>> data;
-
-		public EnumerableDataSource<KeyValuePair<int, double>> Data
-		{
-			get
-			{
-				return data;
-			}
-			set
-			{
-				value.SetXMapping(k => k.Key);
-				value.SetYMapping(k => k.Value);
-				data = value;
-			}
-		}
-
-		public EnumerableDataSource<KeyValuePair<int, double>> setData(double[] data)
-		{
-			ObservableCollection<KeyValuePair<int, double>> inData = new ObservableCollection<KeyValuePair<int, double>>();
-			int i = 0;
-			foreach (double d in data) {
-				inData.Add(new KeyValuePair<int, double>(i, d));
-				i++;
-			}
-
-			EnumerableDataSource<KeyValuePair<int, double>> outData = new EnumerableDataSource<KeyValuePair<int, double>>(inData);
-			outData.SetXMapping(k => k.Key);
-			outData.SetYMapping(k => k.Value);
-			return outData;
-		}
+		public EnumerableDataSource<KeyValuePair<int, double>> Data { get; private set; }
 
 		public override void Handle(Pipeline.TickRenderedMessage message)
 		{
-			Data = new EnumerableDataSource<KeyValuePair<int, double>>(NodeModel.Data.Select((datum, idx) => new KeyValuePair<int, double>(idx, datum)));
+			Data = new EnumerableDataSource<KeyValuePair<int, double>>(
+				NodeModel.Data.Select((datum, idx) => new KeyValuePair<int, double>(idx, datum))
+			);
+			Data.SetXMapping(k => k.Key);
+			Data.SetYMapping(k => k.Value);
 			NotifyOfPropertyChange(() => Data);
 		}
 	}
