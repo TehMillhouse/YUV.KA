46db66191993a11d631ab0b1e44fc536e0537440
PGemander <pgemander@gmail.com>
2012-01-30 20:37:54 +0100

    Add IWindowManagerEx.cs

---
+++ implementation/YuvKA/Caliburn/Micro/IWindowManagerEx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Caliburn.Micro
+{
+	public interface IWindowManagerEx : IWindowManager
+	{
+		void SetOwner(IViewAware owningWindow, IViewAware ownedWindow);
+	}
+}
