TehMillhouse/YUV.KA

commit 9160868aeba120281d8b43e6b7be3b720dfe1abb

[PropertyViewModel] Fix #22

implementation/YuvKA.Implementation/ViewModel/PropertyEditor/PropertyViewModels.cs
@@ -26,7 +26,7 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 				file.FileName = file.FileName.Substring(currentDir.Length + 1);
 			}
 
-			Value = new YuvKA.Pipeline.FilePath(file.FileName);
+			TypedValue = new YuvKA.Pipeline.FilePath(file.FileName);
 		}
 	}
 
@@ -34,8 +34,8 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 	{
 		public Color ChosenColor
 		{
-			get { return Color.FromRgb(Value.R, Value.G, Value.B); }
-			set { Value = new Rgb(value.R, value.G, value.B); }
+			get { return Color.FromRgb(TypedValue.R, TypedValue.G, TypedValue.B); }
+			set { TypedValue = new Rgb(value.R, value.G, value.B); }
 		}
 	}
 
@@ -43,14 +43,14 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 	{
 		public int Width
 		{
-			get { return Value.Width; }
-			set { Value = new Size(value, Height); }
+			get { return TypedValue.Width; }
+			set { TypedValue = new Size(value, Height); }
 		}
 
 		public int Height
 		{
-			get { return Value.Height; }
-			set { Value = new Size(Width, value); }
+			get { return TypedValue.Height; }
+			set { TypedValue = new Size(Width, value); }
 		}
 
 		protected override void OnValueChanged()
@@ -81,8 +81,19 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 	{
 	}
 
-	public class DoublePropertyViewModel : NumericalPropertyViewModel<double>
+	public class NullableDoublePropertyViewModel : NumericalPropertyViewModel<double?>
 	{
+
+		protected override void OnValueChanged()
+		{
+			base.OnValueChanged();
+			NotifyOfPropertyChange(() => SlidersAreEnabled);
+		}
+
+		public bool SlidersAreEnabled
+		{
+			get { return TypedValue != null; }
+		}
 	}
 
 	public class ObservableCollectionOfDoublePropertyViewModel : PropertyViewModel<ObservableCollection<double>>
@@ -93,7 +104,7 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 		{
 			base.OnValueChanged();
 			NotifyOfPropertyChange(() => Wrapper);
-			Value.CollectionChanged += (sender, e) => NotifyOfPropertyChange(() => Wrapper);
+			TypedValue.CollectionChanged += (sender, e) => NotifyOfPropertyChange(() => Wrapper);
 		}
 
 		IEnumerable<DoubleWrapper> wrapperCollection;
@@ -117,8 +128,8 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 		{
 			get
 			{
-				if (wrapperCollection == null || wrapperCollection.Count() != Value.Count()) {
-					wrapperCollection = Value.Select((_, index) => new DoubleWrapper(Value, index)).ToArray();
+				if (wrapperCollection == null || wrapperCollection.Count() != TypedValue.Count()) {
+					wrapperCollection = TypedValue.Select((_, index) => new DoubleWrapper(TypedValue, index)).ToArray();
 					return wrapperCollection;
 				}
 				return wrapperCollection;

implementation/YuvKA/ViewModel/PropertyEditor/PropertyViewModel.cs
@@ -62,6 +62,6 @@ namespace YuvKA.ViewModel.PropertyEditor
 	public abstract class PropertyViewModel<T> : PropertyViewModel
 	{
 		protected PropertyViewModel(bool commitOnValueChanged = true) : base(commitOnValueChanged) { }
-		public new T Value { get { return (T)base.Value; } set { base.Value = (T)value; } }
+		public T TypedValue { get { return (T)base.Value; } set { base.Value = (T)value; } }
 	}
 }

implementation/YuvKA.Implementation/Pipeline/NoiseInputNode.cs
@@ -47,7 +47,17 @@ namespace YuvKA.Pipeline.Implementation
 
 		[DataMember]
 		[Browsable(true)]
-		public NoiseType Type { get; set; }
+		public NoiseType Type
+		{
+			get { return type; }
+			set
+			{
+				type = value;
+				NotifyOfPropertyChange(() => Type);
+				NotifyOfPropertyChange(() => Scale);
+				NotifyOfPropertyChange(() => Speed);
+			}
+		}
 
 		[DataMember]
 		private int Seed;
@@ -55,12 +65,36 @@ namespace YuvKA.Pipeline.Implementation
 		[DataMember]
 		[Browsable(true)]
 		[Range(0.0, 1.0)]
-		public double Speed { get; set; }
+		public double? Speed
+		{
+			get
+			{
+				if (Type == NoiseType.Coherent || Type == NoiseType.ColoredCoherent) {
+					return null;
+				}
+				return speed;
+			}
+			set { speed = value ?? speed; }
+		}
 
 		[DataMember]
 		[Browsable(true)]
 		[Range(0.0, 1.0)]
-		public double Scale { get; set; }
+		public double? Scale
+		{
+			get
+			{
+				if (Type == NoiseType.Coherent || Type == NoiseType.ColoredCoherent) {
+					return null;
+				}
+				return scale;
+			}
+			set { scale = value ?? scale; }
+		}
+
+		double scale;
+		double speed;
+		NoiseType type;
 
 		public override Frame OutputFrame(int tick)
 		{
@@ -112,7 +146,7 @@ namespace YuvKA.Pipeline.Implementation
 			for (int y = 0; y < frame.Size.Height; ++y) {
 				for (int x = 0; x < frame.Size.Width; ++x) {
 					// Generate a noise function value, which is also tick-dependent
-					double randomNumber = (Noise(x * Scale, y * Scale, Speed * tick) + 1) / 2;
+					double randomNumber = (Noise(x * (double)Scale, y * (double)Scale, (double)Speed * tick) + 1) / 2;
 					byte randomColor = (byte)(randomNumber * 255);
 					frame[x, y] = new Rgb(randomColor, randomColor, randomColor);
 				}
@@ -125,9 +159,11 @@ namespace YuvKA.Pipeline.Implementation
 			for (int y = 0; y < frame.Size.Height; ++y) {
 				for (int x = 0; x < frame.Size.Width; ++x) {
 					// Generate noise function values, which is also tick-dependent
-					double randomNumberR = (Noise(x * Scale, y * Scale, Speed * tick) + 1) / 2;
-					double randomNumberG = (Noise((x + 42) * Scale, (y - 42) * Scale, Speed * (tick + 42)) + 1) / 2;
-					double randomNumberB = (Noise((x - 42) * Scale, (y + 42) * Scale, Speed * (tick - 42)) + 1) / 2;
+					double randomNumberR = (Noise(x * (double)Scale, y * (double)Scale, (double)Speed * tick) + 1) / 2;
+					double randomNumberG = (Noise((x + 42) * (double)Scale,
+						(y - 42) * (double)Scale, (double)Speed * (tick + 42)) + 1) / 2;
+					double randomNumberB = (Noise((x - 42) * (double)Scale,
+						(y + 42) * (double)Scale, (double)Speed * (tick - 42)) + 1) / 2;
 					byte randomRed = (byte)(randomNumberR * 255);
 					byte randomGreen = (byte)(randomNumberG * 255);
 					byte randomBlue = (byte)(randomNumberB * 255);

implementation/YuvKA.Implementation/View/PropertyEditor/NullableDoublePropertyView.xaml
@@ -1,4 +1,4 @@
-﻿<UserControl x:Class="YuvKA.View.PropertyEditor.Implementation.DoublePropertyView"
+﻿<UserControl x:Class="YuvKA.View.PropertyEditor.Implementation.NullableDoublePropertyView"
 			xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
 			xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
 			xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
@@ -11,7 +11,7 @@
 		<DockPanel>
 			<TextBlock Text="{Binding Minimum}" DockPanel.Dock="Left" Width="10"/>
 			<TextBlock Text="{Binding Maximum}" DockPanel.Dock="Right" Width="10"/>
-			<Slider Value="{Binding Value}" Maximum="{Binding Maximum}" Minimum="{Binding Minimum}" IsSnapToTickEnabled="True" TickFrequency="0.05" cal:Message.Attach="[Event LostFocus] = [Action CommitChange]"/>
+			<Slider Value="{Binding Value}" IsEnabled="{Binding SlidersAreEnabled}" Maximum="{Binding Maximum}" Minimum="{Binding Minimum}" IsSnapToTickEnabled="True" TickFrequency="0.05" cal:Message.Attach="[Event LostFocus] = [Action CommitChange]"/>
 		</DockPanel>
 		<TextBlock Text="{Binding Value, StringFormat=g5}" HorizontalAlignment="Center" FontSize="10"/>
 	</StackPanel>

implementation/YuvKA.Implementation/YuvKA.Implementation.csproj
@@ -140,7 +140,7 @@
       <Generator>MSBuild:Compile</Generator>
       <SubType>Designer</SubType>
     </Page>
-    <Page Include="View\PropertyEditor\DoublePropertyView.xaml">
+    <Page Include="View\PropertyEditor\NullableDoublePropertyView.xaml">
       <Generator>MSBuild:Compile</Generator>
       <SubType>Designer</SubType>
     </Page>

commit be3ae9b4fc8062a0ce5c07bc1dc5d2a9860217c2

Fix PropertyViewModels (IntPropertyViewModel in particular)
Fix a bug introduced in 9160868aeba120281d8b43e6b7be3b720dfe1abb
which made the use of integer property ViewModels impossible,
potentially breaking other PropertyViewModels too.

implementation/YuvKA.Implementation/ViewModel/PropertyEditor/PropertyViewModels.cs
@@ -26,7 +26,7 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 				file.FileName = file.FileName.Substring(currentDir.Length + 1);
 			}
 
-			TypedValue = new YuvKA.Pipeline.FilePath(file.FileName);
+			Value = new YuvKA.Pipeline.FilePath(file.FileName);
 		}
 	}
 
@@ -34,8 +34,8 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 	{
 		public Color ChosenColor
 		{
-			get { return Color.FromRgb(TypedValue.R, TypedValue.G, TypedValue.B); }
-			set { TypedValue = new Rgb(value.R, value.G, value.B); }
+			get { return Color.FromRgb(Value.R, Value.G, Value.B); }
+			set { Value = new Rgb(value.R, value.G, value.B); }
 		}
 	}
 
@@ -43,14 +43,14 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 	{
 		public int Width
 		{
-			get { return TypedValue.Width; }
-			set { TypedValue = new Size(value, Height); }
+			get { return Value.Width; }
+			set { Value = new Size(value, Height); }
 		}
 
 		public int Height
 		{
-			get { return TypedValue.Height; }
-			set { TypedValue = new Size(Width, value); }
+			get { return Value.Height; }
+			set { Value = new Size(Width, value); }
 		}
 
 		protected override void OnValueChanged()
@@ -92,7 +92,7 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 
 		public bool SlidersAreEnabled
 		{
-			get { return TypedValue != null; }
+			get { return Value != null; }
 		}
 	}
 
@@ -104,7 +104,7 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 		{
 			base.OnValueChanged();
 			NotifyOfPropertyChange(() => Wrapper);
-			TypedValue.CollectionChanged += (sender, e) => NotifyOfPropertyChange(() => Wrapper);
+			Value.CollectionChanged += (sender, e) => NotifyOfPropertyChange(() => Wrapper);
 		}
 
 		IEnumerable<DoubleWrapper> wrapperCollection;
@@ -128,8 +128,8 @@ namespace YuvKA.ViewModel.PropertyEditor.Implementation
 		{
 			get
 			{
-				if (wrapperCollection == null || wrapperCollection.Count() != TypedValue.Count()) {
-					wrapperCollection = TypedValue.Select((_, index) => new DoubleWrapper(TypedValue, index)).ToArray();
+				if (wrapperCollection == null || wrapperCollection.Count() != Value.Count()) {
+					wrapperCollection = Value.Select((_, index) => new DoubleWrapper(Value, index)).ToArray();
 					return wrapperCollection;
 				}
 				return wrapperCollection;

implementation/YuvKA/ViewModel/PropertyEditor/PropertyViewModel.cs
@@ -62,6 +62,6 @@ namespace YuvKA.ViewModel.PropertyEditor
 	public abstract class PropertyViewModel<T> : PropertyViewModel
 	{
 		protected PropertyViewModel(bool commitOnValueChanged = true) : base(commitOnValueChanged) { }
-		public T TypedValue { get { return (T)base.Value; } set { base.Value = (T)value; } }
+		public T Value { get { return (T)base.Value; } set { base.Value = (T)value; } }
 	}
 }