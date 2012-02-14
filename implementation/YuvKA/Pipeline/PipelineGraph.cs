6062c30ea7f36d84700662c321b1caca0d1bab74
PGemander <pgemander@gmail.com>
2012-02-14 22:37:41 +0100

    [PipelineGraph] Add documentation

--- implementation/YuvKA/Pipeline/PipelineGraph.cs
+++ implementation/YuvKA/Pipeline/PipelineGraph.cs
@@ -6,6 +6,10 @@ using Caliburn.Micro;
 
 namespace YuvKA.Pipeline
 {
+
+	/// <summary>
+	/// This class manages the basic functionalities of the pipeline.
+	/// </summary>
 	[DataContract]
 	public class PipelineGraph : PropertyChangedBase
 	{
@@ -14,9 +18,15 @@ namespace YuvKA.Pipeline
 			Nodes = new List<Node>();
 		}
 
+		/// <summary>
+		/// The list of nodes contained in the pipeline.
+		/// </summary>
 		[DataMember]
 		public IList<Node> Nodes { get; private set; }
 
+		/// <summary>
+		/// Represents the number of frames of the longest finit video input in the pipeline. If all inputs have infinite length the value is null.
+		/// </summary>
 		public int? TickCount
 		{
 			get
@@ -26,10 +36,8 @@ namespace YuvKA.Pipeline
 		}
 
 		/// <summary>
-		/// Returns the number of frames to precompute, so the specified nodes can process the next frame. The method assumes, that the graph does not contain any cycles.
+		/// Returns the number of frames which are necessary to let the specified nodes process the next frame. The method assumes, that the graph does not contain any cycles.
 		/// </summary>
-		/// <param name="outputNodes">The specified nodes.</param>
-		/// <returns>The number of frames to precompute.</returns>
 		public int NumberOfFramesToPrecompute(IEnumerable<Node> outputNodes)
 		{
 			int precomputeCount = 0;
@@ -40,7 +48,7 @@ namespace YuvKA.Pipeline
 		}
 
 		/// <summary>
-		/// Returns true iff an edge between the specified nodes does not lead to a cycle and can be added.
+		/// Returns true iff an edge between the specified nodes does not lead to a cycle.
 		/// </summary>
 		public bool CanAddEdge(Node source, Node sink)
 		{
@@ -48,14 +56,12 @@ namespace YuvKA.Pipeline
 		}
 
 		/// <summary>
-		/// Returns true if the specified edge does not lead to a cycle and can be added. Else the method returns false.
+		/// Returns true and adds the edge iff the specified directed edge does not lead to a cycle.
 		/// </summary>
-		/// <param name="source"> The start of the new edge. </param>
-		/// <param name="sink"> The end of the new edge. </param>
-		/// <returns> True if the new edge has been added, else it returns false.</returns>
 		public bool AddEdge(Node.Output source, Node.Input sink)
 		{
 			IEnumerable<Node> nodeList = DepthFirstSearch(source.Node);
+			// if sink can be reached after starting dfs at the source the new edge would create a cycle
 			if (nodeList.Any(node => node.Inputs.Contains(sink))) {
 				return false;
 			}
@@ -66,12 +72,12 @@ namespace YuvKA.Pipeline
 		}
 
 		/// <summary>
-		/// Adds the index to the new node's name. The new name will be the first available one of the row NodeName, NodeName 2, NodeName 3, NodeName 4...
+		/// Adds an index to the specified node's name. The new name will be the first available one of the row NodeName, NodeName 2, NodeName 3, NodeName 4...
 		/// </summary>
-		/// <param name="node">The new node.</param>
 		public void AddNodeWithIndex(Node node)
 		{
 			bool nameIsFree = false;
+			// walk through nodes and look for first free index
 			for (int i = 1; i <= (Nodes.Count + 1) && !nameIsFree; i++) {
 				nameIsFree = true;
 				foreach (Node existingNode in Nodes) {
@@ -82,22 +88,31 @@ namespace YuvKA.Pipeline
 						nameIsFree = false;
 					}
 				}
+				// special case: "NodeName 1" shall be known as "NodeName"
 				if (nameIsFree && i > 1) {
 					node.Name += " " + i;
 				}
 			}
-			if (node is InputNode)
-				node.PropertyChanged += delegate { NotifyOfPropertyChange(() => TickCount); };
+			if (node is InputNode) {
+				NotifyOfPropertyChange(() => TickCount);
+				node.PropertyChanged += (sender, e) => {
+					if (e.PropertyName == "TickCount")
+						NotifyOfPropertyChange(() => TickCount);
+				};
+			}
 			Nodes.Add(node);
 		}
 
+		/// <summary>
+		/// Removes the specified node from the node list and removes all edges connected with the node.
+		/// </summary>
 		public void RemoveNode(Node node)
 		{
 			this.Nodes.Remove(node);
 			foreach (Node checkedNode in Nodes) {
 				foreach (Node.Input potentialDependency in checkedNode.Inputs) {
 					if (potentialDependency.Source != null && potentialDependency.Source.Node == node) {
-						// Delete edge to node
+						// delete edge to node
 						potentialDependency.Source = null;
 					}
 				}
@@ -106,12 +121,8 @@ namespace YuvKA.Pipeline
 		}
 
 		/// <summary>
-		/// Returns a list which contains those Nodes who are necessary to let the specified Node process.
-		/// If the Graph contains a cycle, the returned list will contain one of the cycle-Nodes twice.
-		/// Otherwise every Node in the list is unique.
+		/// Returns a list which contains all nodes which can be reached from the start node.
 		/// </summary>
-		/// <param name="startNode"> The specified Node. </param>
-		/// <returns> Returns the list of necessary Nodes.</returns>
 		public IEnumerable<Node> DepthFirstSearch(Node startNode)
 		{
 			LinkedList<Node> nodeList = new LinkedList<Node>();
@@ -125,6 +136,7 @@ namespace YuvKA.Pipeline
 			return nodeList;
 		}
 
+		// Recursive part of NumberOfFramesToPrecompute.
 		private int NumberOfFramesToPrecompute(Node startNode)
 		{
 			int framesToPrecompute = 0;
@@ -139,9 +151,7 @@ namespace YuvKA.Pipeline
 			return framesToPrecompute;
 		}
 
-		/**
-		 * Recursive part of the depth first search.
-		 */
+		// Recursive part of the depth first search.
 		private void Visit(Node node, LinkedList<Node> nodeList, HashSet<Node> visited)
 		{
 			if (node.Inputs != null) {
@@ -149,8 +159,7 @@ namespace YuvKA.Pipeline
 					if (input.Source != null) {
 						Node child = input.Source.Node;
 						if (nodeList.Contains(child) && visited.Contains(child)) {
-							nodeList.AddLast(child);
-							//Cancel further search here, since a cycle has been found
+							// cancel further search here, since a cycle has been found
 							return;
 						}
 						else if (!nodeList.Contains(child)) {
