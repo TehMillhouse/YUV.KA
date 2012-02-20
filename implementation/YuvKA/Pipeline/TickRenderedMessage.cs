4f42e69eb9fcefbfdf2fd3e85a0323fb1f076db8
Sebastian Ullrich <sebasti@nullrich.de>
2012-02-20 22:30:24 +0100

    [TickRenderedMessage] Document

--- implementation/YuvKA/Pipeline/TickRenderedMessage.cs
+++ implementation/YuvKA/Pipeline/TickRenderedMessage.cs
@@ -3,6 +3,9 @@ using YuvKA.VideoModel;
 
 namespace YuvKA.Pipeline
 {
+	/// <summary>
+	/// Holds the result of rendering a tick with a PipelineDriver: a rendered frame per displayed node output.
+	/// </summary>
 	public class TickRenderedMessage
 	{
 		IDictionary<Node.Output, Frame> dic;
@@ -12,6 +15,9 @@ namespace YuvKA.Pipeline
 			this.dic = dic;
 		}
 
+		/// <summary>
+		/// Gets the frame rendered for the given output or null if the output hasn't been rendered.
+		/// </summary>
 		public Frame this[Node.Output output]
 		{
 			get { return dic.ContainsKey(output) ? dic[output] : null; }
