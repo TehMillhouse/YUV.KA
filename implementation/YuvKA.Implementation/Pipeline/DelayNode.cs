f5195fee67eb16f14a875d953f1f2216a6e81145
Michael Vollmer <michael.vollmer2@gmx.de>
2012-02-21 13:11:31 +0100

    [DelayNode] Add documentation

--- implementation/YuvKA.Implementation/Pipeline/DelayNode.cs
+++ implementation/YuvKA.Implementation/Pipeline/DelayNode.cs
@@ -7,27 +7,43 @@ using YuvKA.VideoModel;
 
 namespace YuvKA.Pipeline.Implementation
 {
+	/// <summary>
+	/// Implements the possibility to delay a Frame.
+	/// </summary>
 	[DataContract]
 	public class DelayNode : Node
 	{
 		Queue<Frame> queue;
 
+		/// <summary>
+		/// Constructs a delaynode with a delay of 0.
+		/// It has one In- and one Output.
+		/// </summary>
 		public DelayNode()
 			: base(inputCount: 1, outputCount: 1)
 		{
 			Name = "Delay";
 		}
 
+		/// <summary>
+		/// The number of indices the Frames shall be delayed.
+		/// </summary>
 		[DataMember]
 		[Range(0.0, 30.0)]
 		[Browsable(true)]
 		public int Delay { get; set; }
 
+		/// <summary>
+		/// The value if the output Frames have vectordata attached.
+		/// </summary>
 		public new bool OutputHasMotionVectors
 		{
 			get { return Inputs.All(input => input.Source != null && input.Source.Node.OutputHasMotionVectors); }
 		}
 
+		/// <summary>
+		/// The value if the output Frames have logdata attached.
+		/// </summary>
 		public new bool OutputHasLogfile
 		{
 			get
@@ -36,6 +52,13 @@ namespace YuvKA.Pipeline.Implementation
 			}
 		}
 
+		/// <summary>
+		/// Delays the input.
+		/// </summary>
+		/// <param name="inputs">An array of Frames, with only the first entry regarded.</param>
+		/// <param name="tick">The index of the Frame which is processes now.</param>
+		/// <returns>An array of Frames, whose only entry is the input that was processes dealy functions calls ago.
+		/// If no such input is available that only entry is a black Frame.</returns>
 		public override Frame[] Process(Frame[] inputs, int tick)
 		{
 			if (queue == null) {
