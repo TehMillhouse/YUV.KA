1dd5af89930bba03bf468cd409dd6b0d5ac81bf3
Michael Vollmer <michael.vollmer2@gmx.de>
2012-02-21 13:15:32 +0100

    [YuvKA.Test] Add tests for the overridden equality operators of Rgb

---
+++ implementation/YuvKA.Test/VideoModel/RgbTest.cs
@@ -0,0 +1,27 @@
+﻿namespace YuvKA.Test.VideoModel
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using Xunit;
+	using YuvKA.Pipeline.Implementation;
+	using YuvKA.VideoModel;
+
+	public class RgbTest
+	{
+		[Fact]
+		public void TestEquality()
+		{
+			Rgb a = new Rgb(1, 2, 3);
+			Rgb b = new Rgb(1, 2, 3);
+			Rgb c = new Rgb(1, 2, 2);
+			Assert.Equal(true, a == b);
+			Assert.Equal(true, a.Equals(b));
+			Assert.Equal(true, a != c);
+			Assert.Equal(false, a == c);
+			Assert.Equal(false, a.Equals(c));
+			Assert.Equal(false, a != b);
+		}
+	}
+}
