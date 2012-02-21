131499ae3003386e4e11d977e0ee2b05cf5ecb7c
Michael Vollmer <michael.vollmer2@gmx.de>
2012-02-21 13:11:56 +0100

    [AdditiveMergeNode] Add documentation

--- implementation/YuvKA.Implementation/Pipeline/AdditiveMergeNode.cs
+++ implementation/YuvKA.Implementation/Pipeline/AdditiveMergeNode.cs
@@ -4,15 +4,29 @@ using YuvKA.VideoModel;
 
 namespace YuvKA.Pipeline.Implementation
 {
+	/// <summary>
+	/// This class implements the possibility to overlay Frames additive.
+	/// </summary>
 	[DataContract]
 	public class AdditiveMergeNode : Node
 	{
+		/// <summary>
+		/// Constructs an AdditiveMergeNode.
+		/// It has a variable number of Inputs and one Output.
+		/// </summary>
 		public AdditiveMergeNode()
 			: base(inputCount: null, outputCount: 1)
 		{
 			Name = "Additive Merge";
 		}
 
+		/// <summary>
+		/// Creates an additive overlay of all inputframes.
+		/// If the frames have different sizes, the maximum width and height are taken and missing data is simulated with black.
+		/// </summary>
+		/// <param name="inputs">The array of Frames which will be added together.</param>
+		/// <param name="tick">The index of the Frame which is processes now.</param>
+		/// <returns>A Framearray whose only Entry has the addition of all RGB values of all inputframes.</returns>
 		public override Frame[] Process(Frame[] inputs, int tick)
 		{
 			Size maxSize = Frame.MaxBoundaries(inputs);
