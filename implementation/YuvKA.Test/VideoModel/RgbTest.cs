deb393b3ccca7a8b43fd427371169f46752eedb5
Michael Vollmer <Michael.Vollmer2@gmx.de>
2012-02-22 21:57:04 +0100

    [YuvKA.Test] Enhance Rgbtests to get 100% coverage

--- implementation/YuvKA.Test/VideoModel/RgbTest.cs
+++ implementation/YuvKA.Test/VideoModel/RgbTest.cs
@@ -16,12 +16,37 @@
 			Rgb a = new Rgb(1, 2, 3);
 			Rgb b = new Rgb(1, 2, 3);
 			Rgb c = new Rgb(1, 2, 2);
+			Rgb d = new Rgb(1, 1, 3);
+			Rgb e = new Rgb(2, 2, 3);
 			Assert.Equal(true, a == b);
 			Assert.Equal(true, a.Equals(b));
 			Assert.Equal(true, a != c);
 			Assert.Equal(false, a == c);
 			Assert.Equal(false, a.Equals(c));
 			Assert.Equal(false, a != b);
+			Assert.Equal(false, a.Equals(null));
+			Assert.Equal(false, a == d);
+			Assert.Equal(false, a == e);
+			Assert.Equal(false, a.Equals(d));
+			Assert.Equal(false, a.Equals(e));
+		}
+
+		[Fact]
+		public void TestHashing()
+		{
+			Rgb a = new Rgb(1, 2, 3);
+			Rgb b = new Rgb(1, 2, 3);
+			Rgb c = new Rgb(1, 2, 2);
+			Assert.Equal(a.GetHashCode(), b.GetHashCode());
+			Assert.NotEqual(a.GetHashCode(), c.GetHashCode());
+		}
+
+		[Fact]
+		public void TestToString()
+		{
+			Rgb a = new Rgb(1, 2, 3);
+			string s = "(R 1 G 2 B 3)";
+			Assert.Equal(s, a.ToString());
 		}
 	}
 }
