89a5563a1bd297918679ec85ec8e67720a45e904
Max Wagner <max.wagner01@gmail.com>
2012-02-22 21:11:22 +0100

    [MacroblockDecision] Add documentation

--- implementation/YuvKA/VideoModel/MacroblockDecision.cs
+++ implementation/YuvKA/VideoModel/MacroblockDecision.cs
@@ -2,10 +2,19 @@
 
 namespace YuvKA.VideoModel
 {
+	/// <summary>
+	/// A class representing the encoder decision per macroblock (16x16 image block)
+	/// </summary>
 	public class MacroblockDecision
 	{
+		/// <summary>
+		/// A vector that specifies where the sample data of this macroblock is taken from in the previous frame.
+		/// </summary>
 		public Vector Movement { get; set; }
 
+		/// <summary>
+		/// The decision concerning partitioning that the encoder took at this macroblock.
+		/// </summary>
 		public MacroblockPartitioning? PartitioningDecision { get; set; }
 	}
 }
