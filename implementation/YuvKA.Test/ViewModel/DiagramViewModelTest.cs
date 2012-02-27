5a6b42ec1f2e1ae915c57b4395988bac9da575e8
Daniel Lebert <D.Lebert@gmx.de>
2012-02-27 17:00:42 +0100

    [DiagramVMTest] Add further tests.

--- implementation/YuvKA.Test/ViewModel/DiagramViewModelTest.cs
+++ implementation/YuvKA.Test/ViewModel/DiagramViewModelTest.cs
@@ -192,5 +192,174 @@ namespace YuvKA.Test.ViewModel
 			Assert.True(gcLoggedRefLog.DisplayTypes.ToList().Exists(type => type.Item2.DependsOnLogfile));
 			Assert.True(gcLoggedRefLog.DisplayTypes.ToList().Exists(type => type.Item2.DependsOnAnnotatedReference));
 		}
+
+		[Fact]
+		public void ResetChosenType()
+		{
+			var vm = MainViewModelTest.GetInstance().PipelineViewModel;
+			var node = new NodeViewModel(new DiagramNode(), vm);
+			vm.Nodes.Add(node);
+			vm.Parent.Model.Graph.AddNode(node.Model);
+
+			var DVM = new DiagramViewModel((DiagramNode)vm.Nodes.Single().Model);
+
+			IoC.GetAllInstances = type => new IGraphType[] { new PixelDiff(), new PeakSignalNoiseRatio() };
+
+			// Add Input Node for DiagramNode with 3 Outputs.
+			var sourceNode = new AnonymousNode(AnonNodeHelper.SourceNode, 3);
+			var inputs = sourceNode.Process(null, 0);
+
+			// Add reference video to node.
+			var reference = new Node.Input { Source = sourceNode.Outputs[0] };
+			DVM.NodeModel.Inputs.Add(reference);
+
+			// Add other Outputs as Inputs to DiagramNode.
+			var video = new Node.Input { Source = sourceNode.Outputs[1] };
+			DVM.NodeModel.Inputs.Add(video);
+
+			DVM.Reference = DVM.Videos[0];
+			DVM.ChosenVideo = DVM.Videos[1];
+			DVM.AddGraphControl();
+			var gc = DVM.GraphControls.Single();
+			gc.ChosenType = gc.DisplayTypes[0];
+
+			DVM.NodeModel.ProcessCore(inputs, 0);
+
+			DVM.Handle(null);
+
+			gc.ChosenType = gc.DisplayTypes[1];
+
+			DVM.NodeModel.ProcessCore(inputs, 1);
+
+			DVM.Handle(null);
+
+			Assert.Equal(gc.DisplayTypes[0].Item2.Process(inputs[1], inputs[0]), gc.Graph.Data[0].Value);
+			Assert.Equal(gc.DisplayTypes[1].Item2.Process(inputs[1], inputs[0]), gc.Graph.Data[1].Value);
+		}
+
+		[Fact]
+		public void ResetReferenceWithExistingGraphControls()
+		{
+			var vm = MainViewModelTest.GetInstance().PipelineViewModel;
+			var node = new NodeViewModel(new DiagramNode(), vm);
+			vm.Nodes.Add(node);
+			vm.Parent.Model.Graph.AddNode(node.Model);
+
+			var DVM = new DiagramViewModel((DiagramNode)vm.Nodes.Single().Model);
+
+			IoC.GetAllInstances = type => new IGraphType[] { new PixelDiff() };
+
+			// Add Input Node for DiagramNode with 3 Outputs.
+			var sourceNode = new AnonymousNode(AnonNodeHelper.SourceNode, 3);
+			var inputs = sourceNode.Process(null, 0);
+
+			// Add reference video to node.
+			var reference = new Node.Input { Source = sourceNode.Outputs[0] };
+			DVM.NodeModel.Inputs.Add(reference);
+
+			// Add other Outputs as Inputs to DiagramNode.
+			var video = new Node.Input { Source = sourceNode.Outputs[1] };
+			DVM.NodeModel.Inputs.Add(video);
+			var altRef = new Node.Input { Source = sourceNode.Outputs[2] };
+			DVM.NodeModel.Inputs.Add(altRef);
+
+			DVM.Reference = DVM.Videos[0];
+			DVM.ChosenVideo = DVM.Videos[1];
+			DVM.AddGraphControl();
+			var gc = DVM.GraphControls.Single();
+			gc.ChosenType = gc.DisplayTypes[0];
+
+			DVM.NodeModel.ProcessCore(inputs, 0);
+
+			DVM.Handle(null);
+
+			DVM.Reference = DVM.Videos[2];
+
+			DVM.NodeModel.ProcessCore(inputs, 1);
+
+			DVM.Handle(null);
+
+			Assert.Equal(gc.DisplayTypes[0].Item2.Process(inputs[1], inputs[0]), gc.Graph.Data[0].Value);
+			Assert.Equal(gc.DisplayTypes[0].Item2.Process(inputs[1], inputs[2]), gc.Graph.Data[1].Value);
+		}
+
+		[Fact]
+		public void CannotAddGCWithoutChosenVid()
+		{
+			var vm = MainViewModelTest.GetInstance().PipelineViewModel;
+			var node = new NodeViewModel(new DiagramNode(), vm);
+			vm.Nodes.Add(node);
+			vm.Parent.Model.Graph.AddNode(node.Model);
+
+			var DVM = new DiagramViewModel((DiagramNode)vm.Nodes.Single().Model);
+
+			IoC.GetAllInstances = type => new IGraphType[] { new PixelDiff() };
+
+			// Add Input Node for DiagramNode with 3 Outputs.
+			var sourceNode = new AnonymousNode(AnonNodeHelper.SourceNode, 3);
+			var inputs = sourceNode.Process(null, 0);
+
+			// Add reference video to node.
+			var reference = new Node.Input { Source = sourceNode.Outputs[0] };
+			DVM.NodeModel.Inputs.Add(reference);
+
+			// Add other Outputs as Inputs to DiagramNode.
+			var video = new Node.Input { Source = sourceNode.Outputs[1] };
+			DVM.NodeModel.Inputs.Add(video);
+			var altRef = new Node.Input { Source = sourceNode.Outputs[2] };
+			DVM.NodeModel.Inputs.Add(altRef);
+
+			DVM.Reference = DVM.Videos[0];
+
+			DVM.AddGraphControl();
+
+			Assert.Empty(DVM.GraphControls);
+		}
+
+		[Fact]
+		public void ChangeAnnReferenceToReferenceWithoutLog()
+		{
+			var vm = MainViewModelTest.GetInstance().PipelineViewModel;
+			var node = new NodeViewModel(new DiagramNode(), vm);
+			vm.Nodes.Add(node);
+			vm.Parent.Model.Graph.AddNode(node.Model);
+
+			var DVM = new DiagramViewModel((DiagramNode)vm.Nodes.Single().Model);
+
+			IoC.GetAllInstances = type => new IGraphType[] { new PixelDiff(), new DecisionDiff() };
+
+			// Add Input Node for DiagramNode with 3 Outputs.
+			var sourceNode = new AnonymousNode(AnonNodeHelper.SourceNode, 3);
+			var inputs = sourceNode.Process(null, 0);
+
+			// Add reference video to node.
+			var reference = new Node.Input { Source = sourceNode.Outputs[0] };
+			DVM.NodeModel.Inputs.Add(reference);
+
+			// Add other Outputs as Inputs to DiagramNode.
+			var video = new Node.Input { Source = sourceNode.Outputs[1] };
+			DVM.NodeModel.Inputs.Add(video);
+
+			var annVid = new Node.Input { Source = sourceNode.Outputs[2] };
+			DVM.NodeModel.Inputs.Add(annVid);
+
+			DVM.Reference = DVM.Videos[0];
+			DVM.ChosenVideo = DVM.Videos[2];
+			DVM.AddGraphControl();
+			var gc = DVM.GraphControls.Single();
+			gc.ChosenType = gc.Types[1];
+
+			DVM.NodeModel.ProcessCore(inputs, 0);
+
+			DVM.Handle(null);
+
+			DVM.Reference = DVM.Videos[1];
+
+			DVM.NodeModel.ProcessCore(inputs, 1);
+
+			DVM.Handle(null);
+
+			Assert.Equal(0.0, gc.Graph.Data[1].Value);
+		}
 	}
 }
