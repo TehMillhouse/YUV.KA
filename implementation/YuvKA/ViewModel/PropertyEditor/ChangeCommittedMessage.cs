66d602de00f97caae0ed32825ec08b9ac3058fec
Sebastian Ullrich <sebasti@nullrich.de>
2012-02-27 19:22:57 +0100

    [ChangeCommittedMessage] Document

--- implementation/YuvKA/ViewModel/PropertyEditor/ChangeCommittedMessage.cs
+++ implementation/YuvKA/ViewModel/PropertyEditor/ChangeCommittedMessage.cs
@@ -1,5 +1,8 @@
 ﻿namespace YuvKA.ViewModel.PropertyEditor
 {
+	/// <summary>
+	/// Message to notify the undo/redo system of a self-contained property change in the pipeline.
+	/// </summary>
 	public class ChangeCommittedMessage
 	{
 	}
