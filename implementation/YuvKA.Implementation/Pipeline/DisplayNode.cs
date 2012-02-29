4b1baec1f46d1ffb217349202157bc8a4e9871a7
Max Wagner <max.wagner01@gmail.com>
2012-02-29 19:58:29 +0100

    [Nodes] Add DisplayNode

---
+++ implementation/YuvKA.Implementation/Pipeline/DisplayNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using YuvKA.VideoModel;
+using YuvKA.ViewModel;
+
+namespace YuvKA.Pipeline.Implementation
+{
+	/// <summary>
+	/// Implements the possibility to delay a Frame.
+	/// </summary>
+	[DataContract]
+	public class DisplayNode : Node
+	{
+
+		/// <summary>
+		/// Constructs a DisplayNode.
+		/// It has one In- and no Outputs.
+		/// </summary>
+		public DisplayNode()
+			: base(inputCount: 1, outputCount: 1)
+		{
+			Name = "Display";
+		}
+
+		[Browsable(true)]
+		public VideoOutputViewModel outputViewModel { get { return new VideoOutputViewModel(this.Outputs[0]); } }
+
+		/// <summary>
+		/// Displays the input.
+		/// </summary>
+		/// <param name="inputs">An array of Frames, with only the first entry regarded.</param>
+		/// <param name="tick">The index of the Frame which is processed now.</param>
+		/// <returns>The Frame it was given.</returns>
+		public override Frame[] Process(Frame[] inputs, int tick)
+		{
+			return inputs;
+		}
+	}
+}
