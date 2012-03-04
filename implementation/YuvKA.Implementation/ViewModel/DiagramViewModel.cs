a19f51e4bcf077c99f95c362e8d1b27a507e56ad
Max Wagner <max.wagner01@gmail.com>
2012-03-04 17:04:20 +0100

    [DiagramView] Fix graph deletion button binding
    Also, reintroduce a using statement that was lost somewhere
    in spite of still being necessary

--- implementation/YuvKA.Implementation/ViewModel/DiagramViewModel.cs
+++ implementation/YuvKA.Implementation/ViewModel/DiagramViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Drawing;
 using System.Linq;
 using Caliburn.Micro;
