1fc5466297f15260037dc8a0c69b94f48b173dcd
Max Wagner <max.wagner01@gmail.com>
2012-03-06 21:32:01 +0100

    Add presentation for the end of the implementation and test phase

---
+++ implementation/presentation/implementation.d
@@ -0,0 +1,150 @@
+# vim: ft=make
+.PHONY: implementation._graphics
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/german/ngerman.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/ifxetex/ifxetex.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/oberdiek/gettitlestring.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/oberdiek/hobsub-generic.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/oberdiek/hobsub-hyperref.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/oberdiek/ifpdf.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/oberdiek/ifvtex.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/oberdiek/infwarerr.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/oberdiek/ltxcmds.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcore.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorearrows.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcoreexternal.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcoregraphicstate.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcoreimage.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorelayers.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcoreobjects.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorepathconstruct.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorepathprocessing.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorepathusage.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorepatterns.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorepoints.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorequick.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcorescopes.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcoreshade.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcoretransformations.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/basiclayer/pgfcoretransparency.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/frontendlayer/tikz/libraries/tikzlibrarytopaths.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/frontendlayer/tikz/tikz.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/libraries/pgflibraryplothandlers.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmath.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathcalc.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfloat.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfunctions.base.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfunctions.basic.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfunctions.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfunctions.comparison.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfunctions.misc.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfunctions.random.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfunctions.round.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathfunctions.trigonometric.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathparser.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/math/pgfmathutil.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/modules/pgfmodulematrix.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/modules/pgfmoduleplot.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/modules/pgfmoduleshapes.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/systemlayer/pgfsys.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/systemlayer/pgfsysprotocol.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/systemlayer/pgfsyssoftpath.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/utilities/pgffor.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/utilities/pgfkeys.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/utilities/pgfkeysfiltered.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/utilities/pgfrcs.code.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/generic/pgf/utilities/pgfutil-common.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/amscls/amsthm.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/amsfonts/amsfonts.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/amsfonts/amssymb.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/amsmath/amsbsy.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/amsmath/amsgen.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/amsmath/amsmath.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/amsmath/amsopn.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/amsmath/amstext.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/base/inputenc.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamer.cls
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaseauxtemplates.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaseboxes.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasecolor.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasecompatibility.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasedecode.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasefont.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaseframe.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaseframecomponents.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaseframesize.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaselocalstructure.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasemisc.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasemodes.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasenavigation.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasenotes.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaseoptions.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaseoverlay.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasercs.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaserequires.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasesection.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasetemplates.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasethemes.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasetheorems.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasetitle.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasetoc.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasetranslator.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbasetwoscreens.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/beamerbaseverbatim.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/themes/color/beamercolorthemedefault.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/themes/font/beamerfontthemedefault.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/themes/inner/beamerinnerthemedefault.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/themes/outer/beamerouterthemedefault.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/themes/theme/beamerthemedefault.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/translator/translator-language-mappings.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/beamer/translator/translator.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/geometry/geometry.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/graphics/graphics.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/graphics/graphicx.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/graphics/keyval.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/graphics/trig.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/hyperref/hyperref.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/hyperref/nameref.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/ms/everyshi.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/oberdiek/epstopdf-base.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/oberdiek/grfext.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/oberdiek/kvoptions.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/oberdiek/rerunfilecheck.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/basiclayer/pgf.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/basiclayer/pgfcore.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/compatibility/pgfcomp-version-0-65.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/compatibility/pgfcomp-version-1-18.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/frontendlayer/tikz.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/systemlayer/pgfsys.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/utilities/pgffor.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/utilities/pgfkeys.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/utilities/pgfrcs.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/pgf/utilities/xxcolor.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/psnfss/helvet.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/psnfss/mathpazo.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/rcs/rcs.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/tools/enumerate.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/tools/verbatim.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/url/url.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: /usr/share/texmf-dist/tex/latex/xcolor/xcolor.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: KIT/KITcolors.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: KIT/KITdefs.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: KIT/beamercolorthemeKIT.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: KIT/beamerfontthemeKIT.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: KIT/beamerinnerthemeKIT.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: KIT/beamerouterthemeKIT.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: KIT/beamerthemeKIT.sty
+implementation.aux implementation.aux.make implementation.d implementation.pdf: disclaimer.tex
+implementation.aux implementation.aux.make implementation.d implementation.pdf: implementation.tex
+.SECONDEXPANSION:
+-include images/logo.png.gpi.d
+implementation.d: $$(call graphics-source,images/logo.png)
+implementation.pdf implementation._graphics: $$(call graphics-target,images/logo.png)
+-include images/Layers.png.gpi.d
+implementation.d: $$(call graphics-source,images/Layers.png)
+implementation.pdf implementation._graphics: $$(call graphics-target,images/Layers.png)
+-include images/gantt.png.gpi.d
+implementation.d: $$(call graphics-source,images/gantt.png)
+implementation.pdf implementation._graphics: $$(call graphics-target,images/gantt.png)
+-include images/sa.pdf.gpi.d
+implementation.d: $$(call graphics-source,images/sa.pdf)
+implementation.pdf implementation._graphics: $$(call graphics-target,images/sa.pdf)
---
+++ implementation/presentation/implementation.toc.make
@@ -0,0 +1,2 @@
+\beamer@endinputifotherversion {3.12pt}
+\beamer@sectionintoc {1}{Architektur\IeC {\"a}nderungen}{2}{0}{1}
