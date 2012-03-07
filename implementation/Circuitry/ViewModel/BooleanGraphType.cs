9b1a6caccc331c2902cecd0c28f963c2f83ac7a8
Sebastian Ullrich <sebasti@nullrich.de>
2012-03-07 12:43:07 +0100

    [Circuitry] Add useless, albeit pretty BooleanGraphType

---
+++ implementation/Circuitry/ViewModel/BooleanGraphType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using YuvKA.Pipeline.Implementation;
+using YuvKA.VideoModel;
+
+namespace Circuitry.ViewModel
+{
+	[DisplayName("Boolean")]
+	public class BooleanGraphType : IGraphType
+	{
+		public bool DependsOnReference { get { return false; } }
+
+		public bool DependsOnAnnotatedReference { get { return false; } }
+
+		public bool DependsOnLogfile { get { return false; } }
+
+		public double Process(Frame frame, Frame reference)
+		{
+			return DigitalNode.FrameToBool(frame) ? 1 : 0;
+		}
+	}
+}
